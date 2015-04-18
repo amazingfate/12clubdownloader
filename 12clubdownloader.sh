9f289e590abe75058f3d8a46f5a4074ae4914f4d
Jianfeng Liu <liujianfeng1994@gmail.com>
2015-04-18 14:10:05 +0800

    Update 12clubdownloader.sh
    fix "Binary file (standard input) matches" output of grep

--- 12clubdownloader.sh
+++ 12clubdownloader.sh
@@ -12,7 +12,7 @@ else
     mkdir $2
 fi
 
-curl $1 | grep -E -o "/program_items/[0-9]*"  > url.txt
+curl $1 | grep -E -o -a "/program_items/[0-9]*"  > url.txt
 while read downloadaddress
 do
         aria2c --dir=$2 -x 3 http://12club.nankai.edu.cn/${downloadaddress}
